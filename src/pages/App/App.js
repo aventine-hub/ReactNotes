83868dbb87ff601a54e0f6f3773f0080ac96f448
Chris L <c.aventine@gmail.com>
2020-10-12 15:30:37 -0500

    JWS

---
+++ src/pages/App/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
+import NavBar from '../../components/NavBar/NavBar';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: userService.getUser()
+    };
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser() });
+  }
+
+  render() {
+    return (
+      <>
+        <header className="App-header">
+          ReactNotes
+          <nav>
+            <NavBar
+              user={this.state.user}
+              handleLogout={this.handleLogout}
+            />
+          </nav>
+        </header>
+        <Switch>
+          <Route exact path='/signup' render={({ history }) =>
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          } />
+          <Route exact path='/login' render={({ history }) =>
+            <LoginPage
+              handleSignupOrLogin={this.handleSignupOrLogin}
+              history={history}
+            />
+          } />
+        </Switch>
+      </>
+    );
+  }
+}
+
+export default App;
