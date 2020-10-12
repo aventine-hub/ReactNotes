a0388208d2a8c1b8b7b6baf16949f06b780c1b9e
Chris L <c.aventine@gmail.com>
2020-10-12 15:52:40 -0500

    Note.js model

---
+++ models/note.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var noteSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    textContent: { type: String, default: 'Write away..' },
+    complete: { type: Boolean, default: false }
+}, {
+    timestamps: true
+});
+module.exports = mongoose.model('Note', noteSchema);
